9c81a53cd5d6c0514a7f64856bdd873aaac4d18e
Yahya-Elomari <elomari.yahya.perso@gmail.com>
2024-05-19 11:05:18 -0700

    2nd commit

--- app/src/main/java/com/example/moviesapi/single_movie_details/SingleMovie.kt
+++ app/src/main/java/com/example/moviesapi/single_movie_details/SingleMovie.kt
@@ -1,21 +1,21 @@
-package com.example.moviesapi
+package com.example.moviesapi.single_movie_details
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.moviesapi.R
 
-class MainActivity : AppCompatActivity() {
+class SingleMovie : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_single_movie)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
